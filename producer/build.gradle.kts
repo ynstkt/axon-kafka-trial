a5cca22610941360b62189caf5f8f51b3b53de20
ynstkt <89331454+ynstkt@users.noreply.github.com>
2024-11-09 14:50:29 +0900

    イベント永続化とKafka送信追加

--- producer/build.gradle.kts
+++ producer/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     val kotlinVersion = "2.0.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
-    id("org.springframework.boot") version "3.3.1"
+    id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.5"
 }
 
@@ -17,6 +17,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.axonframework:axon-spring-boot-starter:4.10.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly ("com.mysql:mysql-connector-j")
+    implementation("org.axonframework.extensions.kafka:axon-kafka-spring-boot-starter:4.10.0")
 }
 
 kotlin {
--- producer/src/main/kotlin/org/example/axonkafkatrial/producer/service/TrialService.kt
+++ producer/src/main/kotlin/org/example/axonkafkatrial/producer/service/TrialService.kt
@@ -1,16 +1,25 @@
 package org.example.axonkafkatrial.producer.service
 
 import org.axonframework.commandhandling.CommandHandler
+import org.axonframework.eventhandling.gateway.EventGateway
 import org.example.axonkafkatrial.producer.command.DocCreate
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
-class TrialService {
+class TrialService(private val eventGateway: EventGateway) {
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     @CommandHandler
     fun run(command: DocCreate) {
         logger.info("command accepted: $command")
+        val event = DocCreated(command.docId, command.body)
+        eventGateway.publish(event)
     }
 }
+
+data class DocCreated(
+    val docId: UUID,
+    val body: String,
+)
