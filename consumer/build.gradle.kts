a272c2bec60e80244de5f2200179fb2dfafd05ca
ynstkt <89331454+ynstkt@users.noreply.github.com>
2024-11-10 10:28:08 +0900

    consumer追加

---
+++ consumer/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    val kotlinVersion = "2.0.0"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
